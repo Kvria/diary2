81ee2985004e8bfc89444f6e300665bc28e7d3db
Kvria <trishakuria@gmail.com>
2020-03-30 15:25:25 +0300

    fresh start

--- diary/src/app/activity.ts
+++
@@ -1,9 +0,0 @@
-export class activity {
-    id: number;
-  name: string;
-  description: string;
-    showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string){
-      this.showDescription=false;
-    }
-  }
--- diary/src/app/app.component.ts
+++ diary/src/app/app.component.ts
@@ -9,12 +9,4 @@ import { Activity} from './activity';
 export class AppComponent {
   title = 'diary';
   
-  diary:Activity[] = [
-    {id:1,name:'Attend meeting'},
-    {id:2,name:'Got to the office'},
-    {id:3,name:'Reply to emails'},
-    {id:4,name:'Do house shopping'},
-    {id:5,name:'School meeting '},
-    {id:6,name:'Pick children from school'},
-  ];
 }
